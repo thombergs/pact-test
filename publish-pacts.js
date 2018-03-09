a4481ec0d50a465d828e25ee3e90f5edd429be8d
hombergs <hombergs@adesso.de>
2018-03-09 14:52:05 +0100

    Fehlersuche in Pact Files

--- publish-pacts.js
+++ publish-pacts.js
@@ -8,29 +8,51 @@
 const BROKER_URL = 'http://pact.remondis-de.local/';
 
 let project = require('./package.json');
-let projectFolder = __dirname;
+let pactFolder = __dirname + '/pacts';
 let pact = require('@pact-foundation/pact-node');
 let tag = process.argv[2];
 
 if (tag === undefined) {
   console.error(
-    "Please provide a tag for the published Pact as command line argument!")
+      "Please provide a tag for the published Pact as command line argument!")
   process.exit(1);
 }
 
-let opts = {
-  pactFilesOrDirs: [projectFolder + '/pacts'],
-  pactBroker: BROKER_URL,
-  consumerVersion: project.version,
-  tags: [tag]
+replaceSpecialCharacters();
+publishPact(pactFolder + '/ui-article.json');
 
-  // aktuell erfordert der Pact Broker keine Credentials
-  // pactBrokerUsername: <String>,
-  // pactBrokerPassword: <String>,
-};
+/**
+ * Veröffentlicht die Pacts auf dem Pact Broker.
+ */
+function publishPact(filename) {
+  let options = {
+    pactFilesOrDirs: [filename],
+    pactBroker: BROKER_URL,
+    consumerVersion: project.version,
+    tags: [tag]
 
-pact.publishPacts(opts).then(function () {
-  console.log("Pacts successfully published with the tag '" + tag + "'!");
-}, function () {
-  process.exit(1);
-});
+    // aktuell erfordert der Pact Broker keine Credentials
+    // pactBrokerUsername: <String>,
+    // pactBrokerPassword: <String>,
+  };
+
+  pact.publishPacts(options).then(function () {
+    console.log("Pact " + filename + " successfully published with the tag '" + tag + "'!");
+  }, function () {
+    console.log("Error while publishing pact " + filename + "!");
+  });
+}
+
+/**
+ * Ersetzt Sonderzeichen in den Pact-Files, mit denen Pact-Node nicht zurechtkommt.
+ */
+function replaceSpecialCharacters() {
+  var fs = require('fs');
+  var files = fs.readdirSync(pactFolder);
+  files.forEach(function (file) {
+    file = pactFolder + '/' + file;
+    var data = fs.readFileSync(file, 'utf8');
+    var result = data.replace(/€/g, '\\u20ac');
+    fs.writeFileSync(file, result, 'utf8');
+  });
+}
